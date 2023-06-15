c0cc1d37acf14223da3e4ba4a1fcc05cc4277b2e
AaronSichoneDev <aaron.sichone@northrise.net>
2023-06-15 20:08:06 +0200

    Initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,6 +14,10 @@
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to Vuetify
         </h1>
+        <h1 class="display-2 font-weight-bold mb-3">
+         I love programming
+        </h1>
+
 
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
